281bb6037f9b6f913a8386dcfca1634adcd10379
Val Do <val@Vals-iMac.local>
2017-08-29 15:57:04 +0400

    Components && mixisns && gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ var gulp = require('gulp'),
     minifyCSS = require('gulp-minify-css'),
     gulpIgnore = require('gulp-ignore'),
     htmlmin = require('gulp-htmlmin'),
+    gutil = require('gulp-util'),
     rename = require('gulp-rename'),
     watch = require('gulp-watch');
 
@@ -19,7 +20,7 @@ gulp.task('sass', function() {
         .src('assets/sass/**/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
-        .pipe(sourcemaps.write())
+        .pipe(sourcemaps.write('../build/sourcemaps'))
         .pipe(gulp.dest('assets/css/src'))
 });
 
@@ -29,8 +30,10 @@ gulp.task('sass', function() {
 gulp.task('minify-css', function() {
     return gulp
         .src('assets/css/src/**/*.css')
+        .pipe(sourcemaps.init({ loadMaps: true }))
         .pipe(minifyCSS({ keepBreaks: false }))
         .pipe(rename({ suffix: '.min' }))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('assets/css/build'))
 });
 //////////////////////////////////////////////////////////////
@@ -39,7 +42,8 @@ gulp.task('minify-css', function() {
 gulp.task('compress-js', function() {
     return gulp.src('assets/js/src/**/*.js')
         .pipe(gulpIgnore.exclude(false))
-        .pipe(uglify())
+        .pipe(sourcemaps.write('sourcemaps'))
+        .pipe(uglify().on('error', gutil.log))
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('assets/js/build'))
 });
@@ -48,7 +52,7 @@ gulp.task('compress-js', function() {
 // watcher
 //////////////////////////////////////////////////////////////
 gulp.task('watcher', function() {
-    //sass
+    //sass 
     gulp.watch('assets/sass/**/*.scss', ['sass']);
     gulp.watch('assets/css/src/**/*.css', ['minify-css']);
     gulp.watch('assets/js/src/**/*.js', ['compress-js']);
